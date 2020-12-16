15ee2cb70f2dbc65b1106115e0f676c62e0d113c
Birzhan Serik <birzhans14@gmail.com>
2020-12-16 14:51:07 +0600

    impl. data insertion

--- data_insertion.py
+++ data_insertion.py
@@ -2,28 +2,28 @@ import csv
 from car.models import *
 
 with open("dataset.csv") as f:
-    reader = csv.reader(f, sep="\t")
+    reader = csv.reader(f, delimiter='\t')
     next(reader)
-    c = 0
-    while c < 5:
-        for row in reader:
-            print(row)
-            # _, created = Car.objects.get_or_create(
-            #     id=c,
-            #     brand=row[0],
-            #     model=row[1],
-            #     year=int(row[2]),
-            #     city=row[3],
-            #     body_type=row[4],
-            #     engine_volume=round(float(row[5]), 1),
-            #     mileage=int(row[6]),
-            #     gear_type=row[7],
-            #     steering_wheel=row[8],
-            #     color=row[9],
-            #     wheel_drive=row[10],
-            #     custom_clearanced=True if "TRUE" == row[11] else False,
-            #     price=int(row[12]),
-            #     average_price=int(row[13]),
-            #     link=row[14],
-            #     fuel_type=row[15],
-            # )
+    c = 10001
+    for row in reader:
+        while c < 19000:
+            _, created = Car.objects.get_or_create(
+                id=c,
+                brand=row[1],
+                model=row[2],
+                year=int(row[3]),
+                city=row[4],
+                body_type=row[5],
+                engine_volume=round(float(row[6]), 1),
+                mileage=int(row[7]),
+                gear_type=row[8],
+                steering_wheel=row[9],
+                color=row[10],
+                wheel_drive=row[11],
+                custom_clearanced=True if "TRUE" == row[12] else False,
+                price=int(row[13]),
+                average_price=int(row[14]),
+                link=row[15],
+                fuel_type=row[16],
+            )
+            c += 1
