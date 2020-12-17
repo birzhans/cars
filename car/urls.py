c269fea66057807f778f96004328821b5644c578
Birzhan Serik <birzhans14@gmail.com>
2020-12-18 01:44:58 +0600

    edit header & add static pages

--- car/urls.py
+++ car/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 from .views import index, car_detail
 
 urlpatterns = [
-    path("", index),
+    path("", index, name="car_index_path"),
     path("<str:id>/", car_detail, name="car_detail_url"),
 ]
--- cars/urls.py
+++ cars/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from .views import index
+from .views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index),
+    path("", index, name="index_path"),
+    path("about/", about, name="about_path"),
     path("car/", include("car.urls")),
 ]
